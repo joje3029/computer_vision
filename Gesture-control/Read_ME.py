cf580634f278b729ded8b5f451c46bfd12d7d8b6
joje3029 <127838357+joje3029@users.noreply.github.com>
2023-11-09 20:23:22 +0900

    Read_me 파일 위치 똑바로해서 다시 커밋

---
+++ Gesture-control/Read_ME.py
@@ -0,0 +1,11 @@
+# 미디어 파이프
+# https://developers.google.com/mediapipe/solutions/vision/gesture_recognizer
+
+# 코드 클론
+# git clone https://github.com/leelang7/Gesture-control.git
+
+# 실행 명령어 main.py를 실행해야 함으로 : python main.py
+
+# 실행전 설치할 것
+# pip install opencv-python==4.8.1.78
+# pip install mediapipe==0.8.4.2   
